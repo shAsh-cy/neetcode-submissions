ab394f11f60e104a173203c8040b6450ff57d335
shAsh-cy <126700402+shAsh-cy@users.noreply.github.com>
2026-06-12 18:02:40 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_s = sorted(s)
+        t_t = sorted(t)
+        if s_s == t_t:
+            return True
+        return False
